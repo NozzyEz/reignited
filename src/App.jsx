22a64e7d3a0d045dc842bd72d5e4a3c7c0757bc8
Mark Sahlgreen <mark@nozzy.org>
2021-03-26 09:06:40 +0100

    added dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Reignited</h1>
-      </header>
+      <h1>Hello Reignited</h1>
     </div>
   );
 }
